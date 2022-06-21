8aec618a227099a2b56b974883a5217fdb1be165
Благушин Ростислав Фёдорович <Blagushin.R.F@sberbank.ru>
2022-06-21 16:40:47 +0300

    init commit

---
+++ src/main/java/com/example/emptyspringproject/EmptySpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emptyspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptySpringProjectApplication.class, args);
+    }
+
+}
